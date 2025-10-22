a8fb1448eefd08abd8ba8f26d21a077197cffd21
Eduardo - Pessoal <dumedolago@gmail.com>
2025-10-22 17:39:56 -0300

    inicio da implementação de um endpoint de estatisticas + testes

--- jogos/urls.py
+++ jogos/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
 
     path('jogos/', views.ListCreateJogoView.as_view(), name='jogos-create-list'),
     path('jogos/<int:pk>', views.RetrieveUpdateDestroyJogoView.as_view(), name='jogos-detail'),
+    path('jogos/stats/', views.JogosStatsView.as_view(), name='jogos-stats-view'),
 
 ]
--- jogos/views.py
+++ jogos/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, views, response, status 
 from jogos.models import Jogo
 from jogos.serializers import JogoSerializer
 from rest_framework.permissions import IsAuthenticated
@@ -13,3 +13,14 @@ class RetrieveUpdateDestroyJogoView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthenticated, ViewPermission)
     queryset = Jogo.objects.all()
     serializer_class = JogoSerializer
+
+class JogosStatsView(views.APIView):
+    permission_classes = (IsAuthenticated, ViewPermission)
+
+    def get(self, request):
+        return response.Response(
+            data= {
+                "message": "funcionou"
+            },
+            status=status.HTTP_200_OK
+        )
