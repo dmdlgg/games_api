8a5dcfa7ad8381e76b3a365a3e9f533008c5415f
Eduardo - Pessoal <dumedolago@gmail.com>
2025-10-23 17:27:05 -0300

    implementação do endpoint de esatisticas da api

--- jogos/views.py
+++ jogos/views.py
@@ -1,3 +1,5 @@
+from django.db.models import Count 
+from reviews.models import Review
 from rest_framework import generics, views, response, status 
 from jogos.models import Jogo
 from jogos.serializers import JogoSerializer
@@ -16,11 +18,19 @@ class RetrieveUpdateDestroyJogoView(generics.RetrieveUpdateDestroyAPIView):
 
 class JogosStatsView(views.APIView):
     permission_classes = (IsAuthenticated, ViewPermission)
+    queryset = Jogo.objects.all()
 
     def get(self, request):
+        total_jogos = self.queryset.count()
+        jogos_por_genero = self.queryset.values('genero__nome').annotate(count=Count('id'))
+        total_reviews = Review.objects.count()
+
+
         return response.Response(
             data= {
-                "message": "funcionou"
+                "total_jogos": total_jogos,
+                "jogos_por_generos": jogos_por_genero,
+                "tota_reviews": total_reviews
             },
             status=status.HTTP_200_OK
         )
